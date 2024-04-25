299c65a47e6dba95157abf1f2ae0148058034fb1
Muhammad Qamar <qamarmehar.0301@gmail.com>
2024-04-25 22:34:57 +0500

    Search_Screen

--- src/Global/Data.js
+++ src/Global/Data.js
@@ -1,66 +1,66 @@
 
 //This Data is for Small Card used in Catagory Section
 export const filterData = [
-    { name: "Biryani", image: require('../assets/biryani.jpeg'), id: "0" },
-    { name: "Kabab", image: require('../assets/kabab.jpeg'), id: "1" },
-    { name: "Zinger Burger", image: require('../assets/zinger.jpg'), id: "2" },
-    { name: "Shawarma", image: require('../assets/shawrama.jpeg'), id: "3" },
-    { name: "Sandwich", image: require('../assets/sandwich.jpeg'), id: "4" },
-    { name: "Loaded Fries", image: require('../assets/loader_fries.jpg'), id: "5" },
-    { name: "Fried Chicken", image: require('../assets/fried_chicken.jpg'), id: "6" }
+    { name: "Biryani ", image: require('../assets/biryani.jpeg'), id: "0" },
+    { name: "Kabab ", image: require('../assets/kabab.jpeg'), id: "1" },
+    { name: "Zinger Burger ", image: require('../assets/zinger.jpg'), id: "2" },
+    { name: "Shawarma ", image: require('../assets/shawrama.jpeg'), id: "3" },
+    { name: "Sandwich ", image: require('../assets/sandwich.jpeg'), id: "4" },
+    { name: "Loaded Fries ", image: require('../assets/loader_fries.jpg'), id: "5" },
+    { name: "Fried Chicken ", image: require('../assets/fried_chicken.jpg'), id: "6" },
 ]
 
 //This Data is for Fodd Card used in Delivery Section
 export const resturantsData = [
     {
-        resturantsName: "Nisar Charsi Tikka", farAway: "5", businessAddress: "Model Town Lahore",
+        resturantsName: "Nisar Charsi Tikka ", farAway: "5", businessAddress: "Model Town Lahore ",
         images: "https://i.dawn.com/primary/2019/01/5c2dd06bbbe42.jpg", coordinate: { lat: -26.2423, lng: 34.523423 },
         averageReviews: 4.5, numberofReviews: 244, disscount: 50, deliveryTime: 15, collectTime: 15, foodType: "Chiken Karahi, Beef Karahi , Kabab ",
         ProductData: [
-            { name: "Chicken Karahi", price: 1600 , image: "https://img.freepik.com/free-photo/grilled-meat-vegetables-cast-iron-homemade-generated-by-ai_188544-20585.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1712966400&semt=ais" },
-            { name: "Kabab", price: 350, image: "https://www.shutterstock.com/image-photo/grilled-lula-kebab-on-skewers-260nw-1820685599.jpg" },
-            { name: "BBQ", price: 190, image: "https://t4.ftcdn.net/jpg/03/36/59/67/360_F_336596714_KYxkCzJK686f0lon80WIeHOecR3OIy5S.jpg" },
-            { name: "Malai Boti", price: 250, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvnJLLiLQ8KB_8dHNFlvwQIkJkKebcso-De-2UAk5rJg&s" },
+            { name: "Chicken Karahi ", price: 1600 , image: "https://img.freepik.com/free-photo/grilled-meat-vegetables-cast-iron-homemade-generated-by-ai_188544-20585.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1712966400&semt=ais" },
+            { name: "Kabab ", price: 350, image: "https://www.shutterstock.com/image-photo/grilled-lula-kebab-on-skewers-260nw-1820685599.jpg" },
+            { name: "BBQ ", price: 190, image: "https://t4.ftcdn.net/jpg/03/36/59/67/360_F_336596714_KYxkCzJK686f0lon80WIeHOecR3OIy5S.jpg" },
+            { name: "Malai Boti ", price: 250, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvnJLLiLQ8KB_8dHNFlvwQIkJkKebcso-De-2UAk5rJg&s" },
         ],
         id: 0
     },
     {
-        resturantsName: "Arif Chatkhara", farAway: "7", businessAddress: "Androon Lahore",
+        resturantsName: "Arif Chatkhara ", farAway: "7", businessAddress: "Androon Lahore ",
         images: "https://lh5.googleusercontent.com/proxy/OqaA0apuRViIQL9-SYP73HnD4A7RZnHOHlsAWv2oPDQplNBaMusGBEwwSPpZEMo9Wls59iD7gG0ENCU4pesIuOisN9Hvx7cZuoYLQzU9pJUcDrHVoKM",
         coordinate: { lat: -26.2423, lng: 34.523423 }, averageReviews: 4.9, numberofReviews: 244, disscount: 50, deliveryTime: 15,
         collectTime: 15, foodType: "Tawa Piece, Fried Naan, Karachi",
         ProductData: [
-            { name: "Tawa Piece", price: 750, image: "https://www.shutterstock.com/image-photo/spicy-chicken-tawa-piece-fry-600nw-1198801801.jpg" },
-            { name: "Chicken Karahi", price: 1900 , image: "https://www.shutterstock.com/image-photo/mutton-karahi-restaurant-style-gosht-260nw-2148912765.jpg" },
-            { name: "Fried Naan", price: 50, image: "https://media.istockphoto.com/id/1150376593/photo/bread-tandoori-indian-cuisine.jpg?s=612x612&w=0&k=20&c=GGT5LN7G4zLhJTEnP_KcyvYuayi8f1nJcvQwvmj0rCM=" },
+            { name: "Tawa Piece ", price: 750, image: "https://www.shutterstock.com/image-photo/spicy-chicken-tawa-piece-fry-600nw-1198801801.jpg" },
+            { name: "Chicken Karahi ", price: 1900 , image: "https://www.shutterstock.com/image-photo/mutton-karahi-restaurant-style-gosht-260nw-2148912765.jpg" },
+            { name: "Fried Naan ", price: 50, image: "https://media.istockphoto.com/id/1150376593/photo/bread-tandoori-indian-cuisine.jpg?s=612x612&w=0&k=20&c=GGT5LN7G4zLhJTEnP_KcyvYuayi8f1nJcvQwvmj0rCM=" },
         ],
         id: 1
     },
     {
-        resturantsName: "Smoke & Squads", farAway: "10", businessAddress: "Near UET Lahore",
+        resturantsName: "Smoke & Squads ", farAway: "10", businessAddress: "Near UET Lahore ",
         images: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOMk2iJO073GVIjWpYulm2S-4UoLlmLe2RCjemaKNHGRSeFCHzs_eyOgjNSAN6Hb11SS8&usqp=CAU",
         coordinate: { lat: -26.2423, lng: 34.523423 }, averageReviews: 4.2, numberofReviews: 244, disscount: 50, deliveryTime: 15,
         collectTime: 15, foodType: "Zinger Burger, Andy wala Burger, Chiken Shawarma,Loaded Fries",
         ProductData: [
-            { name: "Zinger Burger", price: 300, image: "https://t4.ftcdn.net/jpg/05/67/61/35/360_F_567613541_rABRpKAjdD3Hyo3e3ebVEx7VHBiF5kHI.jpg" },
-            { name: "Beef Burger", price: 700, image: "https://t3.ftcdn.net/jpg/05/62/77/30/360_F_562773014_AUOqIArlxgOunTfEgK2Ncj2YfEz0qaYb.jpg" },
-            { name: "Loaded Fries", price: 600, image: "https://t4.ftcdn.net/jpg/03/67/72/79/360_F_367727961_q7z5IXobgSnTdUVZiqxSx64aH4RaRsc7.jpg" },
-            { name: "Chicken Shawarma", price: 200, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8qUS0kMLeo-LzCNiESL6rLXf8a-mcfBmWl4puoebHBT0vvMTagE5Nxpb0lCnpkGr0ZD4&usqp=CAU" },
-            { name: "Zinger Shawarma", price: 420, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeu7Ge2cg3Sl78kuo-KtM_XSL0IhH-nOaN-RxSzIi0u8c60RIUxSYwOwTnkd3pzvkfRlU&usqp=CAU" },
-            { name: "Andy Wala Burger", price: 150, image: "https://d3o2ptabp7rtgq.cloudfront.net/bss/webp/media/catalog/product/cache/0382bfe7b144d6408e0d2797b01c42e4/b/u/burger_shami_2.webp" },
-            { name: "Sandwich", price: 220, image: "https://t3.ftcdn.net/jpg/01/11/28/82/360_F_111288255_yYufRCRFaLsxVIlaAQMWYMTJD1R4Dy02.jpg" },
+            { name: "Zinger Burger ", price: 300, image: "https://t4.ftcdn.net/jpg/05/67/61/35/360_F_567613541_rABRpKAjdD3Hyo3e3ebVEx7VHBiF5kHI.jpg" },
+            { name: "Beef Burger ", price: 700, image: "https://t3.ftcdn.net/jpg/05/62/77/30/360_F_562773014_AUOqIArlxgOunTfEgK2Ncj2YfEz0qaYb.jpg" },
+            { name: "Loaded Fries ", price: 600, image: "https://t4.ftcdn.net/jpg/03/67/72/79/360_F_367727961_q7z5IXobgSnTdUVZiqxSx64aH4RaRsc7.jpg" },
+            { name: "Chicken Shawarma ", price: 200, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8qUS0kMLeo-LzCNiESL6rLXf8a-mcfBmWl4puoebHBT0vvMTagE5Nxpb0lCnpkGr0ZD4&usqp=CAU" },
+            { name: "Zinger Shawarma ", price: 420, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeu7Ge2cg3Sl78kuo-KtM_XSL0IhH-nOaN-RxSzIi0u8c60RIUxSYwOwTnkd3pzvkfRlU&usqp=CAU" },
+            { name: "Andy Wala Burger ", price: 150, image: "https://d3o2ptabp7rtgq.cloudfront.net/bss/webp/media/catalog/product/cache/0382bfe7b144d6408e0d2797b01c42e4/b/u/burger_shami_2.webp" },
+            { name: "Sandwich ", price: 220, image: "https://t3.ftcdn.net/jpg/01/11/28/82/360_F_111288255_yYufRCRFaLsxVIlaAQMWYMTJD1R4Dy02.jpg" },
         ], 
         id: 2
     },
     {
-        resturantsName: "Savour Food", farAway: "3", businessAddress: "Lakshmi Chowk Lahore",
+        resturantsName: "Savour Food ", farAway: "3", businessAddress: "Lakshmi Chowk Lahore ",
         images: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHr7Ta6USraGA9cuSjebDjLq4gAX4qOpTq6aLICqJ-aaMvrVrVjrepMVe7Xn42oejSB8M&usqp=CAU",
         coordinate: { lat: -26.2423, lng: 34.523423 }, averageReviews: 4.1, numberofReviews: 244, disscount: 50, deliveryTime: 15,
         collectTime: 15, foodType: "Desi and Fast Food + Deserts also",
         ProductData: [
-            { name: "Biryani", price: 250, image: "https://media-cdn.tripadvisor.com/media/photo-s/0b/0f/ca/c5/savour-foods-pulao-kabab.jpg" },
-            { name: "Mutton Karahi", price: 4500 , image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCTSZZc2xdkNF96UyzqU0w_NNPIAzhRtmSiMPdlNuGFg&s" },
-            { name: "Ice Cream", price: 400 , image: "https://media.gettyimages.com/id/157472912/photo/ice-cream-composition-on-a-bowl.jpg?s=612x612&w=gi&k=20&c=AniWX1OhaarUxCkgjUoKiA3bKVllK0upCylW6Z0PCMQ=" },
+            { name: "Biryani ", price: 250, image: "https://media-cdn.tripadvisor.com/media/photo-s/0b/0f/ca/c5/savour-foods-pulao-kabab.jpg" },
+            { name: "Mutton Karahi ", price: 4500 , image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCTSZZc2xdkNF96UyzqU0w_NNPIAzhRtmSiMPdlNuGFg&s" },
+            { name: "Ice Cream ", price: 400 , image: "https://media.gettyimages.com/id/157472912/photo/ice-cream-composition-on-a-bowl.jpg?s=612x612&w=gi&k=20&c=AniWX1OhaarUxCkgjUoKiA3bKVllK0upCylW6Z0PCMQ=" },
         ],
         id: 3
     },
--- src/screens/Search_Screen.js
+++ src/screens/Search_Screen.js
@@ -1,17 +1,81 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, Image, Alert, TouchableOpacity, ScrollView, FlatList, Pressable, Dimensions } from 'react-native';
+import { Text, View, StyleSheet, FlatList, TouchableWithoutFeedback, ImageBackground, Dimensions } from 'react-native';
 import { Icon } from "react-native-elements";
+import Search_Component from "../Components/Seacrh_Component";
+import { filterData } from "../Global/Data";
+import { colors } from "../Global/styles";
 
-export default function Search_Screen(){
-    return(
-        <View style = {styles.container}> 
-            <Text style = {{color: 'black'}}> This is the Search Screen </Text>
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+export default function Search_Screen() {
+    return (
+        <View style={styles.container}>
+            <Search_Component />
+            
+            <FlatList
+                style={styles.flatList}
+                data={filterData}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <TouchableWithoutFeedback>
+                        <View style={styles.imageView}>
+                            <ImageBackground
+                                style={styles.image_bg}
+                                source={item.image}
+                            >
+                                <View style={styles.textView}>
+                                    <Text style={{ color: 'white' }}>{item.name}</Text>
+                                </View>
+                            </ImageBackground>
+                        </View>
+                    </TouchableWithoutFeedback>
+                )}
+                horizontal={false}
+                showsHorizontalScrollIndicator={false}
+                numColumns={2}
+                ListHeaderComponent={<Text style={styles.listHeader}>Top Categories</Text>}
+            />
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
-    }
-})
+        flex: 1,
+        padding: 10,
+        backgroundColor: 'white',
+    },
+    flatList: {
+        flex: 1,
+        marginTop: '-160%',
+    },
+    listHeader: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        color: colors.grey5,
+        paddingLeft: '3%'
+    },
+    imageView: {
+        flex: 1,
+        margin: 5,
+        borderRadius: 10,
+        overflow: 'hidden',
+        width: SCREEN_WIDTH * 0.4475,
+        height: SCREEN_WIDTH * 0.4475,
+    },
+    image_bg: {
+        flex: 1,
+        borderRadius: 10,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    textView: {
+        backgroundColor: 'rgba(52,52,52,0.3)',
+        padding: 10,
+        width: '100%',
+        alignItems: 'center',
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+    },
+});
